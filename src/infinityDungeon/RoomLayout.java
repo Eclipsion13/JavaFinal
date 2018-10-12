1ec79d76fd702524deb223307ceaabc18142c430
Jason Wells <eclipsion013@outlook.com>
2018-10-12 09:14:45 -0500

    Added buttons to button with

---
+++ src/infinityDungeon/RoomLayout.java
@@ -0,0 +1,34 @@
+package infinityDungeon;
+import javax.swing.*;
+import java.awt.*;
+public class RoomLayout extends JFrame
+{
+	private JButton Left = new JButton("Left Door");
+	private JButton Right = new JButton("Right Door");
+	private JButton Up = new JButton("Inventory");
+	private JButton Down = new JButton("Exit");
+	private JButton Chest = new JButton("Open Chest");
+	private Container con = getContentPane();
+	
+	public RoomLayout()
+	{
+	setLayout(new BorderLayout());
+	con.add(Left, BorderLayout.WEST);
+	con.add(Right, BorderLayout.EAST);
+	con.add(Up, BorderLayout.NORTH);
+	con.add(Down, BorderLayout.SOUTH);
+	con.add(Chest, BorderLayout.CENTER);
+	setSize(350, 250);
+	
+	setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+	public static void dispRoom() 
+	{
+		RoomLayout rm = new RoomLayout();
+		rm.setVisible(true);
+
+		
+		// Book
+	}
+
+}
