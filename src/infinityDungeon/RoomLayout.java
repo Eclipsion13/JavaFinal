a595d3c31c1c4446c1912da96f68dd998877b1e1
Burton428 <ADAMBURTON428@users.noreply.github.com>
2018-10-15 09:51:18 -0500

    update

--- src/infinityDungeon/RoomLayout.java
+++ src/infinityDungeon/RoomLayout.java
@@ -5,26 +5,27 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 public class RoomLayout extends JFrame implements ActionListener
 {
+	JLabel label = new JLabel("THing");
 	private JButton Left = new JButton("Left Door");
 	private JButton Right = new JButton("Right Door");
-	private JButton Inventory = new JButton("Inventory");
 	private JButton Exit = new JButton("Exit");
 	private JButton Chest = new JButton("Open Chest");
 	private Container con = getContentPane();
 	
 	public RoomLayout()
 	{
+	
 	setLayout(new BorderLayout());
+	label.setFont(new Font("Arial", Font.ITALIC,22));
 	con.add(Left, BorderLayout.WEST);
 	con.add(Right, BorderLayout.EAST);
-	con.add(Inventory, BorderLayout.NORTH);
 	con.add(Exit, BorderLayout.SOUTH);
 	con.add(Chest, BorderLayout.CENTER);
 	Left.addActionListener(this);
 	Right.addActionListener(this);
-	Inventory.addActionListener(this);
 	Exit.addActionListener(this);
 	Chest.addActionListener(this);
+	
 	
 	setSize(350, 250);
 	
@@ -45,10 +46,6 @@ public class RoomLayout extends JFrame implements ActionListener
 		{
 			RoomSetup.setup();
 		
-		}
-		else if(source == Inventory) 
-		{
-			
 		}
 		else if(source == Chest) 
 		{
